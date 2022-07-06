7e7bb49fa376da4a6b17f5b93c7731ac9eaf0f8c
ajit kerle <ajitkerle70@gmail.com>
2022-07-06 13:01:10 +0530

    minor changes Model created

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require('body-parser');
+const route=require('./routes/route')
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose
+  .connect(
+    "mongodb+srv://radon-AnandSharma:Anand570@cluster0.riu1k.mongodb.net/group7Database?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+    }
+  )
+
+  .then(() => console.log("MongoDB is Connected."))
+  .catch((err) => console.log(err));
+
+app.use("/", route);
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Express is running on port " + (process.env.PORT || 3000));
+});
---
+++ src/models/reviewModel.js
@@ -0,0 +1,36 @@
+const mongoose=require('mongoose')
+const ObjectId =mongoose.Schema.Types.ObjectId
+
+const reviewSchema=new mongoose.Schema({ 
+  bookId:{
+    type:ObjectId,
+    ref:'Book',
+    required:true
+  },
+  reviewedBy:{
+    type:String,
+    required:true,
+    default:"Guest"
+  },
+  reviewedAt:{
+    type:Date,
+    default:new Date(),
+    required:true
+  },
+  rating:{
+    type:Number,
+    min:1,
+    max:5,
+    required:true
+  },
+  review:{
+    type:String,
+    default:null
+  },
+  isDeleted:{
+    type:Boolean,
+    default:false
+  }
+  },{timestamps:true})
+
+  module.exports=mongoose.model('Review',reviewSchema)
