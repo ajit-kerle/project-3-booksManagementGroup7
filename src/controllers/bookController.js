93b2679b85add313e5019b4ce3ffb0ecf5890b4a
Anand Sharma <sharmaanand570@gmail.com>
2022-07-07 13:42:42 +0530

    update

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -66,47 +66,44 @@ const createBook = async function (req, res) {
 
 const getBooks = async function (req, res) {
     try {
-        if (!Object.keys(req.query)) {
-            return res.status(400).send({ status: false, message: "data is required to filter Book" })
-        }
-        const {userId, category, subcategory} = req.query
-        const filterBook = {isDelated: false}
-        if(userId || userId==""){
-            if(!mongoose.Types.ObjectId.isValid(userId)){
+        const { userId, category, subcategory } = req.query
+        const filterBook = { isDelated: false }
+        if (userId || userId == "") {
+            if (!mongoose.Types.ObjectId.isValid(userId)) {
                 return res.status(400).send({ status: false, message: "Invalid userId" })
             }
             filterBook["userId"] = userId
         }
-        if(category){
+        if (category) {
             filterBook["category"] = category
         }
-        if(subcategory){
+        if (subcategory) {
             const subcategoryArr = subcategory.trim().split(',').map(subcategory => subcategory.trim())
             filterBook['subcategory'] = { $all: subcategoryArr }
         }
-        let returnBooks = await bookModel.find(filterBook)
-        if (!returnBooks) {
+        let returnBooks = await bookModel.find(filterBook).select({ _id: 1, title: 1, excerpt: 1, userId: 1, category: 1, reviews: 1, releasedAt: 1 }).sort({ title: 1 })
+        if (Object.keys(returnBooks).length == 0) {
             return res.status(404).send({ status: false, message: " No book available show" })
         }
         else {
-            let books = {
-                _id: returnBooks._id,
-                title: returnBooks.title,
-                excerpt: returnBooks.excerpt,
-                category: returnBooks.category,
-                reviews: returnBooks.reviews,
-                releasedAt: returnBooks.releasedAt
-            }
-            returnBooks["books"] = books
-            res.status(200).send({ status: true, message: 'Books list', data: books })
+            res.status(200).send({ status: true, message: 'Books list', data: returnBooks })
         }
-
     } catch (err) {
         res.status(500).send({ status: false, message: err.message })
     }
 }
 
+const updateBookById = async function (req, res) {
+    try{
+
+    }
+    catch(error){
+        return res.status(500).send({status:false, message:error.message})
+    }
+}
+
 module.exports = {
     createBook,
-    getBooks
+    getBooks,
+    updateBookById
 }
